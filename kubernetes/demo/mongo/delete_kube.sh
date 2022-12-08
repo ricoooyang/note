12a1804cd30f00f1df671969afa28e9a4c2b0307
rico <a0955383566@gmail.com>
2022-12-08 16:49:06 +0800

    add delete mongo kube sh

---
+++ kubernetes/demo/mongo/delete_kube.sh
@@ -0,0 +1,6 @@
+kubectl delete -f mongo-ingress.yaml
+kubectl delete -f mongo-express.yaml
+kubectl delete -f mongodb.yaml
+kubectl delete -f mongo-configmap.yaml
+kubectl delete -f mongodb-secret.yaml
+
